06d63fa0f65094aa4a6fe416474771bc16f433a8
David <david.vernon@ger911.com>
2022-12-02 23:12:35 -0500

    delete config

--- rds.js
+++ rds.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 
 var rdsUrl = 'vya-aurora-database-instance-1.czbwpwm83uag.us-east-1.rds.amazonaws.com';
-var password =  'passworD&7';
+var password =  ;
 var user = 'admin';
 
 // mysql connection pool
